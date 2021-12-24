57ab090113c75a8acc822606eb598d68871402eb
HarnamSaggu <atoastytoasteronacoaster@gmail.com>
2021-12-24 22:04:56 +0000

    Added generators for number set and target

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,21 @@
 fun main(args: Array<String>) {
-	println("Hello World!")
-
-	// Try adding program arguments via Run/Debug configuration.
-	// Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-	println("Program arguments: ${args.joinToString()}")
+	println(getNumbers(2))
+	println(getTarget())
 }
+
+fun getTarget(): Int = (0 until 1000).random()
+
+fun getNumbers(bigNumberCount: Int): List<Int> {
+	val smallBois = ((1..10) + (1..10)).toMutableList()
+	val bigBois = intArrayOf(25, 50, 75, 100)
+	smallBois.shuffle()
+	bigBois.shuffle()
+	return bigBois.take(bigNumberCount) + smallBois.take(6 - bigNumberCount)
+}
+
+/*
+Take 0-4 big numbers, the other numbers small = 6 numbers
+Either add, sub, mult, div them to make the target 3-digit number
+	sub: cannot produce a negative number
+	div: must be a perfect divisor
+ */
